290359bd2d7f327f530248f16d85d04e5e170160
Rodolpho Reis <rodolphoreis@live.com>
2024-10-20 19:13:35 +0100

    feat(server): add basic Express server with CORS

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3028;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
