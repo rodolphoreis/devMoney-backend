3d05801351d524421f964e4e5ee0b3bd5758f7fb
Rodolpho Reis <rodolphoreis@live.com>
2024-10-20 19:57:15 +0100

    fix: bug in nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
 import express from "express";
 import cors from "cors";
+import prisma from "../prisma/prismaClient";
 
 const app = express();
-const port = 3028;
-
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const port = 3333;
+
+app.get("/transactions", async (req, res) => {
+  try {
+    const transactions = await prisma.transaction.findMany();
+    res.json(transactions);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error fetching transactions" });
+  }
+});
+
+app.post("/transactions", async (req, res) => {
+  const { id, description, amount, type, createdAt, updatedAt } = req.body;
+  try {
+    const newTransaction = await prisma.transaction.create({
+      data: {
+        id,
+        description,
+        amount,
+        type,
+        createdAt,
+        updatedAt,
+      },
+    });
+    res.status(201).json(newTransaction);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error creating transaction" });
+  }
 });
 
 app.listen(port, () => {
